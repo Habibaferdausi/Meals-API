b094e86477307ddf5b3b155a5d57098da55dc9ef
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-04-21 14:33:32 +0600

    update

--- js/meal.js
+++ js/meal.js
@@ -10,18 +10,16 @@ const displayMeals = (meals) => {
   const mealsContainer = document.getElementById("meals-container");
   mealsContainer.innerText = "";
   meals.slice(0, 6).forEach((meal) => {
-    console.log(meal);
-
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
 
     mealDiv.innerHTML = `
-      <div class="card h-100">
-          <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card h-50">
+          <img src="${meal.strMealThumb}" class="img-fluid" alt="...">
           <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <button onclick="loadMealDetail2(${meal.idMeal})"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+              <button onclick="loadMealDetail2(${meal.idMeal})"  type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#mealDetails">
                   Details
               </button>
           </div>
@@ -63,7 +61,7 @@ const displayMealDetails = (meal) => {
   document.getElementById("mealDetailsLabel").innerText = meal.strMeal;
   const mealsDetails = document.getElementById("mealDetailsBody");
   mealsDetails.innerHTML = `
-      <img class="img-fluid" src="${meal.strMealThumb}"> `;
+      <img class="h-20 w-75" src="${meal.strMealThumb}"> `;
 
   const category = document.getElementById("mealDetails2");
   category.innerHTML = `<p class="card-text"> <span class = "fw-bold">Category : </span> ${meal.strCategory}</p> `;
@@ -76,6 +74,14 @@ const displayMealDetails = (meal) => {
 
   const youtube = document.getElementById("youtube");
   youtube.innerHTML = `<p class="card-text"> <span class = "fw-bold">Youtube : </span>  <a href="${meal.strYoutube}" class="stretched-link text-danger">Click to go Youtube</a></p> `;
+};
+
+const showAllTogether = () => {
+  const url =
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data));
 };
 
 loadMeals("chicken");
